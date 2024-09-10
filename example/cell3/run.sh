cd11d4628e8a9824311772ec71db9f09a2aa7103
SkyGrel19 <skibav2002@gmail.com>
2024-09-10 18:26:27 +0300

    update to use STM32F446RET in examples

--- example/cell3/run.sh
+++ example/cell3/run.sh
@@ -1,2 +1,2 @@
 cargo run --release &&
-probe-rs attach --chip AT32F437VMT7 --probe 1366:0105 ../target/thumbv7em-none-eabihf/release/cell2 --no-location
+probe-rs attach --chip STM32F446RETx --probe 1366:0105 ../target/thumbv7em-none-eabihf/release/cell2 --no-location
